c6713f080fe75c8c186fbef2cda9abac4e8cc491
tknott95 <tknott95@hotmail.com>
2017-03-05 04:32:43 -0700

    Deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :api_blog, ApiBlog.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "trevorknott-middleware.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("jajakkjjhk6262hehw8298989")
+
+
+# Configure your database
+config :api_blog, ApiBlogHelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("postgresql-cubed-51511"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +69,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,6 +6,8 @@ defmodule ApiBlog.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
+
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
