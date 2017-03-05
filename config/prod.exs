9f77fdab9810227c002c43f9f117ce53d84a6aa0
tknott95 <tknott95@hotmail.com>
2017-03-05 04:55:36 -0700

    Heroku Setup

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :api_blog, ApiBlog.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "trevorknott-middleware.herokuapp.com", port: 443],
+  url: [host: "trevorknott-middleware.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
@@ -23,7 +23,7 @@ config :api_blog, ApiBlog.Endpoint,
 config :api_blog, ApiBlogHelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),-
   ssl: true
 
 
