c59b828acf40ec457c8375f4fa2194d2977d8202
tknott95 <tknott95@hotmail.com>
2017-03-05 12:09:51 -0700

    Deps Added

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :api_blog, ApiBlog.Endpoint,
 
 
 # Configure your database
-config :api_blog, ApiBlogHelloPhoenix.Repo,
+config :api_blog, ApiBlog.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
